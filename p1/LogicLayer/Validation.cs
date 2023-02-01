7de2c5dddab096a66f3d4a7a4f5deca6abed3f82
manojkmj11 <mithuna692@gmail.com>
2023-02-02 01:58:20 +0530

    User input validation testing

--- p1/LogicLayer/Validation.cs
+++ p1/LogicLayer/Validation.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace LogicLayer
 {
     public class Validation
     {
+        public static bool IsValidEmail(string str)
+        {
+            
+            string pattern = @"^[\w.]{6,30}[^.](@{0})(gmail|outlook|yahoo).com$";
+            if (!Regex.IsMatch(str, pattern, RegexOptions.IgnoreCase))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool IsValidPassword(string str)
+        {
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$";
+            if (!Regex.IsMatch(str, pattern, RegexOptions.IgnoreCase))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
--- p1/Test/TestingValidation.cs
+++ p1/Test/TestingValidation.cs
@@ -1,16 +1,34 @@
+using LogicLayer;
 namespace Test
 {
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
         }
-
+        [TestCase("xyzmail.com", false)]
+        [TestCase("xyz@mail", false)]
+        [TestCase("xyz#@mail.com", false)]
+        [TestCase("xyz@@gmail.com", false)]
+        [TestCase("xyz-@gmail.com", false)]
+        [TestCase("xyz_@gmail.com", false)]
+        [TestCase("xyz!@gmail.com", false)]
+        [TestCase("xyz$@gmail.com", false)]
+        [TestCase("xyz.@gmail.com", false)]
+        [TestCase("xyz.@.gmail.com", false)]
+        [TestCase("xyz@#gmail.com", false)]
+        [TestCase("xyz@.gmail.com", false)]
+        [TestCase("xyz.@gmail.com", false)]
+        [TestCase("xyz@gmail.com.", false)]
+        [TestCase("xyz@gmail.com!", false)]
+        [TestCase("xyz@gmail.com#", false)]
         [Test]
-        public void Test1()
+        public void Test1(string str, bool expected)
         {
-            Assert.Pass();
+            bool actual = Validation.IsValidEmail(str);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
